e66b50375bcffa6e9fff4d37ba273dfbaf8a56ca
tmclean85 <trevor.a.mclean@gmail.com>
2019-05-02 11:23:37 -0700

    Trim match data and filter to correct participant

--- app/routes/summonerData.js
+++ app/routes/summonerData.js
@@ -19,7 +19,9 @@ module.exports = function(app) {
     const player = req.query.summoner;
     const summoner = await getSummoner(player);
     const matches = await getMatches(summoner.accountId)
-    res.status(200).send(matches)
+    const trimmedMatches = await trimData(matches.matches)
+    const matchesData = await getMatchData(player, trimmedMatches)
+    res.status(200).send(matchesData)
   })
 
   async function getSummoner(name) {
@@ -30,6 +32,35 @@ module.exports = function(app) {
   async function getMatches(id) {
     const matches = await kayn.Matchlist.by.accountID(id)
     return matches
+  }
+
+  async function trimData(matchData) {
+    let matchArr = [];
+    for(let i = 0; i < 5; i++) {
+      if(matchData[i])matchArr.push(matchData[i])
+    }
+    return matchArr
+  }
+
+  async function getMatchData(name, matchArray) {
+    // console.log('name: ', name)
+    // console.log('data: ', matchArray)
+    let dataArr = [];
+    for (let i = 0, len = matchArray.length; i < len; i++) {
+      let thisMatch = await kayn.Match.get(matchArray[i].gameId);
+      // compare summoner name against names from participant data to determine summoner's participant ID
+      let participantId = thisMatch.participantIdentities.filter((participants) => {
+        // console.log('data: ', participants)
+        return participants.player.summonerName === name;
+      })
+      // compare summoner's participant ID against match data participant ID to retrieve summoner's match data
+      let matchData = thisMatch.participants.filter((participants) => {
+        // console.log('data: ', participants)
+        return participants.participantId === participantId[0].participantId
+      })
+      dataArr.push({ matchId: matchArray[i].gameId, matchData: matchData })
+    }
+    return dataArr;
   }
 
 }
