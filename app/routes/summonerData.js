a5afac18f86de4beb8e0cc5206494ed4f0362bb5
tmclean85 <trevor.a.mclean@gmail.com>
2019-05-02 10:52:13 -0700

    Implement Kayn, first API call to Summoner

--- app/routes/summonerData.js
+++ app/routes/summonerData.js
@@ -1,5 +1,29 @@
 module.exports = function(app) {
-  app.get('/', (req, res) => {
-    res.status(200).send('Mic check')
+
+  const { Kayn, REGIONS } = require('kayn')
+  const kayn = Kayn('RGAPI-2a66f04c-0517-4307-9012-108f1b405ba8')({
+    region: REGIONS.NORTH_AMERICA,
+    locale: 'en_US',
+    debugOptions: {
+      isEnabled: true,
+      showKey: false,
+    },
+    requestOptions: {
+      shouldRetry: true,
+      numberOfRetriesBeforeAbort: 3,
+      delayBeforeRetry: 1000,
+    }
+  });
+
+  app.get('/', async (req, res) => {
+    const player = req.query.summoner;
+    const summoner = await getSummoner(player);
+    res.status(200).send(summoner)
   })
+
+  async function getSummoner(name) {
+    const summonerDetails = await kayn.Summoner.by.name(name)
+    return summonerDetails
+  };
+
 }
